708a23a586809cb748d6ace87f5b3731a511ce66
Corbin Bartsch <corbin.bartsch@protonmail.com>
2019-01-24 00:00:19 -0500

    Remove unnecessary wal files

--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0E0E0A";
-static const char* selfgcolor   = "#d6d6d7";
-static const char* normbgcolor  = "#79856A";
-static const char* normfgcolor  = "#d6d6d7";
-static const char* urgbgcolor   = "#817B78";
-static const char* urgfgcolor   = "#d6d6d7";
